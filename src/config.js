30740e4bdba95aede7016c3447a030760dd0c866
= <=>
2020-10-31 10:12:50 +0530

    add: initial setup

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const config = {
+    apiKey: "AIzaSyAdtBIlbOxD19Atr2vozqjfrxxgEYa2mgU",
+    authDomain: "academic-ocean-283009.firebaseapp.com",
+    databaseURL: "https://academic-ocean-283009.firebaseio.com",
+}
+
+export default config;
